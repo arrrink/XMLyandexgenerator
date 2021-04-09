21144d6105bcc6a10051504e43f8fc690bac732b
arrrink <arina.nefa@gmail.com>
2021-04-09 22:09:49 +0300

    Initial Commit

---
+++ Shared/XMLyandexgeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMLyandexgeneratorApp.swift
+//  Shared
+//
+//  Created by Арина Нефёдова on 09.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct XMLyandexgeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
